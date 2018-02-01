44af244bfbe934e9707413bcd2e44ae869ad3700
raqeeb.uddin <raqeeb.uddin@PC-00602>
2018-02-01 17:18:20 +0000

    Implemented WebApiStationFinder/ Needs to be tested/ Viewmodel for the landing page needs to be created

---
+++ TfrRedo.WebApi/Queries/WebApiStationFinder.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using Domain.Stations;
+using Newtonsoft.Json;
+using TfrRedo.Services.Interfaces;
+using TfrRedo.Services.SearchStations.Queries.GetStationDetails;
+
+namespace TfrRedo.WebApi.Queries
+{
+    public class WebApiStationFinder : iWebApiStationFinder
+    {
+        public StationDetailModel GetStationDetails(Station statiion)
+        {
+            var _restUri = String.Format(
+                    $"https://api.tfl.gov.uk/Stoppoint/Search/{statiion.Name}?modes=tube&useMultiModalCall=false");
+            using (WebClient client = new WebClient())
+            {
+                var json = client.DownloadString(_restUri);
+                var searchDetails = JsonConvert.DeserializeObject<StationDetailModel>(json);
+
+                return searchDetails;
+            }
+        }
+    }
+}

9cdf568835d61e092a387e1c87631c692c8dff6d
raqeeb.uddin <raqeeb.uddin@PC-00602>
2018-02-09 10:58:43 +0000

    Deleted: about/contact view page Implmented: Ninject binding for IPreviousJourneys

--- TfrRedo/App_Start/NinjectWebCommon.cs
+++ TfrRedo/App_Start/NinjectWebCommon.cs
@@ -58,6 +58,7 @@ namespace TfrRedo.App_Start
                 kernel.Bind<iWebApiJourneyFinder>().To<WebApiJourneyFinder>();
                 kernel.Bind<IJourneyDetailsPageViewModel>().To<JourneyDetailsPageViewModel>();
                 kernel.Bind<IDatabaseService>().To<DatabaseService>();
+                kernel.Bind<IPreviousJourneysViewModel>().To<PreviousJourneysViewModel>();
 
 
 

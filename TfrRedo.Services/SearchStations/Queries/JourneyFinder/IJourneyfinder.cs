0a307f558896491acdba3f3baada64eb6d247038
raqeeb.uddin <raqeeb.uddin@PC-00602>
2018-02-09 09:33:18 +0000

    GetAlljourneys method added to  Services .journeyfinder

--- TfrRedo.Services/SearchStations/Queries/JourneyFinder/IJourneyfinder.cs
+++ TfrRedo.Services/SearchStations/Queries/JourneyFinder/IJourneyfinder.cs
@@ -1,15 +1,11 @@
-﻿using Domain.Stations;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Collections.Generic;
 using TFR.Data.Models.Journey;
 
 namespace TfrRedo.Services.SearchStations.Queries.JourneyFinder
 {
     public interface IJourneyfinder
     {
-       Journey Get(string departureStationIcsId, string arrivalStationIcsId);
+        Journey Get(string departureStationIcsId, string arrivalStationIcsId);
+        List<Journey> GetAllJourneys();
     }
 }
--- TfrRedo.Services/SearchStations/Queries/JourneyFinder/JourneyFinder.cs
+++ TfrRedo.Services/SearchStations/Queries/JourneyFinder/JourneyFinder.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using Domain.Stations;
+﻿using System.Collections.Generic;
 using TFR.Data.Models.Journey;
 using TfrRedo.Services.Interfaces;
 
@@ -12,20 +7,27 @@ namespace TfrRedo.Services.SearchStations.Queries.JourneyFinder
     public class JourneyFinder : IJourneyfinder
     {
         private readonly iWebApiJourneyFinder _webApiJourneyFinder;
-        private IDatabaseService _databaseService;
-        public JourneyFinder(
-            iWebApiJourneyFinder webApiJourneyFinder, IDatabaseService databaseService)
+        private readonly IDatabaseService _databaseService;
+
+        public JourneyFinder(iWebApiJourneyFinder webApiJourneyFinder, IDatabaseService databaseService)
         {
             _webApiJourneyFinder = webApiJourneyFinder;
             _databaseService = databaseService;
         }
-        public  Journey Get(string departureStationIcsId, string arrivalStationIcsId)
+
+        public Journey Get(string departureStationIcsId, string arrivalStationIcsId)
         {
             var journeyDetailsAsync = _webApiJourneyFinder.JourneyFinder(departureStationIcsId, arrivalStationIcsId);
             var journeyDetails = journeyDetailsAsync.Result.Journeys[0];
             _databaseService.Save(journeyDetails);
-            var journeys = _databaseService.AllJourneys();
+           
             return journeyDetails;
+        }
+
+        public List<Journey> GetAllJourneys()
+        {
+            var journeys = _databaseService.AllJourneys();
+            return journeys;
         }
     }
 }

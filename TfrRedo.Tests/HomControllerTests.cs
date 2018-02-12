6cf2502c806691ec31290cdd60bbf13ca34be80c
raqeeb.uddin <raqeeb.uddin@PC-00602>
2018-02-12 17:15:14 +0000

    Begining testing phase Nunit and Moq installed

---
+++ TfrRedo.Tests/HomControllerTests.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using TfrRedo.Controllers;
+
+namespace TfrRedo.Tests
+{
+    [TestFixture]
+    public class HomControllerTests
+    {
+        [Test]
+        [Category("HomeControllerIndexMethodGet")]
+        public void ShoudReturnIndexPageViewModel()
+        {
+        }
+
+        [Test]
+        [Category("HomeControllerIndexMethodGet")]
+        public void ShouldReturnEmptyIndexPageVieWModel()
+        {
+
+        }
+
+        [Test]
+        [Category("HomeControllerIndexMethodGet")]
+        public void ShouldDirectToIndexView()
+        {
+
+        }
+    }
+}

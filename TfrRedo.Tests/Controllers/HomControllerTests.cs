8ac4e3352e4ba7ab032c3165df47e3561750e8a8
raqeeb.uddin <raqeeb.uddin@PC-00602>
2018-02-14 10:31:38 +0000

    Created again

---
+++ TfrRedo.Tests/Controllers/HomControllerTests.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Domain.Stations;
+using Moq;
+using NUnit.Framework;
+using TfrRedo.Controllers;
+using TfrRedo.Services.Interfaces;
+using TfrRedo.Services.SearchStations.Queries.stationFinder;
+using TfrRedo.ViewModels;
+using TfrRedo.WebApi.Queries;
+using Assert = Microsoft.VisualStudio.TestTools.UnitTesting.Assert;
+
+namespace TfrRedo.Tests
+{
+    [TestFixture]
+    public class HomControllerTests
+    {
+        //[SetUp]
+        //static void SetUp()
+        //{
+        //    Mock<HomeController> homeController = new Mock<HomeController>();
+        //}
+
+        //[TearDown]
+       
+        //static void TearDown()
+        //{
+
+        //}
+
+        
+
+        [Test]
+        [Category("HomeControllerIndexMethodGet")]
+        public void ShoudReturnIndexPageViewModel()
+        {
+            var mockIndexPageViewModel = new Mock<IIndexPageViewModel>();
+            IIndexPageViewModel indexPageViewModel, IStationFinder stationFinder,
+            IStationFinderResultPageViewModel stationFinderResultPageViewModel, IJourneyfinder journeyFinder,
+            IJourneyDetailsPageViewModel journeyDetailsPageViewModel, IPreviousJourneysViewModel p
+            var homeController = new HomeController(mockIndexPageViewModel);
+            var stat = new StationFinder();
+            int one = 1;
+
+            Assert.AreEqual(one, 1);
+        }
+    }
+}
---
+++ TfrRedo.Tests/Services/ServicesTests.cs
@@ -0,0 +1,40 @@
+﻿//using Domain.Stations;
+//using Moq;
+//using NUnit.Framework;
+//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
+//using TfrRedo.Services.Interfaces;
+//using TfrRedo.Services.SearchStations.Queries.stationFinder;
+
+//namespace TfrRedo.Tests
+//{
+//    [TestFixture]
+//    public class ServicesTests
+//    {
+//        [Test]
+//        [Category("HomeControllerIndexMethodGet")]
+//        public void ShoudReturnIndexPageViewModel()
+//        {
+//            var _webApiStationFinder = new Mock<IWebApiStationFinder>();
+
+//            var sut = new StationFinder(_webApiStationFinder.Object);
+
+//            var departure = new IndexLandingPageModel
+//            {
+//                Name = "start"
+//            };
+
+//            var arrival = new IndexLandingPageModel
+//            {
+//                Name = "kings"
+//            };
+
+//            var test = sut.Get(departure, arrival);
+
+//            _webApiStationFinder.Verify(x => x.StationFinder(It.IsAny<IndexLandingPageModel>()));
+//        }
+//    }
+//}

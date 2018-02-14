ae6600eed2ccc6ab83fcbda0ebf4c8e4f565e9ee
raqeeb.uddin <raqeeb.uddin@PC-00602>
2018-02-14 13:09:49 +0000

    Test: ShouldReturnIndexViewPageViewModel

--- TfrRedo.Tests/Controllers/HomeControllerTest.cs
+++ TfrRedo.Tests/Controllers/HomeControllerTest.cs
@@ -1,9 +1,60 @@
-﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+﻿
+using System.Web.Mvc;
+using Moq;
+using NUnit.Framework;
+
+using TfrRedo.Controllers;
+using TfrRedo.Services.SearchStations.Queries.JourneyFinder;
+using TfrRedo.Services.SearchStations.Queries.stationFinder;
+using TfrRedo.ViewModels;
 
 namespace TfrRedo.Tests.Controllers
 {
-    [TestClass]
+    [TestFixture]
     public class HomeControllerTest
     {
+        private IIndexPageViewModel _mockIndexViewPageModel;
+        private IStationFinder _mockStationFinder;
+        private IStationFinderResultPageViewModel _mockStationFinderResultPageModel;
+        private IJourneyfinder _mockJourneyFinder;
+        private IJourneyDetailsPageViewModel _mockJourneyDetailsPageViewModel;
+        private IPreviousJourneysViewModel _mockPreviousJourneyViewModel;
+
+        //[SetUp]
+        //private void SetUp()
+        //{
+        //    _mockIndexViewPageModel = new Mock<IIndexPageViewModel>().Object;
+        //    _mockStationFinder = new Mock<IStationFinder>().Object;
+        //    _mockStationFinderResultPageModel = new Mock<IStationFinderResultPageViewModel>().Object;
+        //    _mockJourneyFinder = new Mock<IJourneyfinder>().Object;
+        //    _mockJourneyDetailsPageViewModel = new Mock<IJourneyDetailsPageViewModel>().Object;
+        //    _mockPreviousJourneyViewModel = new Mock<IPreviousJourneysViewModel>().Object;
+
+        //}
+
+        [Test]
+        public void ShouldReturnIndexViewPageViewModel()
+        {
+            _mockIndexViewPageModel = new Mock<IIndexPageViewModel>().Object;
+            _mockStationFinder = new Mock<IStationFinder>().Object;
+            _mockStationFinderResultPageModel = new Mock<IStationFinderResultPageViewModel>().Object;
+            _mockJourneyFinder = new Mock<IJourneyfinder>().Object;
+            _mockJourneyDetailsPageViewModel = new Mock<IJourneyDetailsPageViewModel>().Object;
+            _mockPreviousJourneyViewModel = new Mock<IPreviousJourneysViewModel>().Object;
+
+            var sut = new HomeController(
+                _mockIndexViewPageModel,
+                _mockStationFinder, _mockStationFinderResultPageModel, _mockJourneyFinder,
+                _mockJourneyDetailsPageViewModel, _mockPreviousJourneyViewModel);
+
+
+            var result = sut.Index();
+
+            Assert.That(result, Is.TypeOf<ViewResult>());
+
+        }
+
     }
+
 }
+

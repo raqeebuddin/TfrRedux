16139146f407c06df626ef91b6458055f4c17b1d
raqeeb.uddin <raqeeb.uddin@PC-00602>
2018-02-21 11:52:49 +0000

    Ninject implmented in WebConfig/ connection to Content bundle has been lost

--- TfrRedo/Global.asax.cs
+++ TfrRedo/Global.asax.cs
@@ -1,18 +1,76 @@
-﻿using System.Web;
+﻿using System;
+using System.Web;
 using System.Web.Mvc;
 using System.Web.Optimization;
 using System.Web.Routing;
+using Ninject;
+using Ninject.Web.Common;
+using Ninject.Web.Common.WebHost;
+using TfrRedo.DataAccess;
+using TfrRedo.Services.Interfaces;
+using TfrRedo.Services.SearchStations.Queries.JourneyFinder;
+using TfrRedo.Services.SearchStations.Queries.stationFinder;
+using TfrRedo.ViewModels;
+using TfrRedo.WebApi.Queries;
 
 namespace TfrRedo
 {
-    public class MvcApplication : HttpApplication
+    public class MvcApplication : NinjectHttpApplication
     {
-        protected void Application_Start()
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
         {
+            filters.Add(new HandleErrorAttribute());
+        }
+
+        public static void RegisterRoutes(RouteCollection routes)
+        {
+            routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+
+            routes.MapRoute(
+                "Default", // Route name
+                "{controller}/{action}/{id}", // URL with parameters
+                new
+                {
+                    controller = "Home",
+                    action = "Index",
+                    id = UrlParameter.Optional
+                });
+        }
+
+        protected override IKernel CreateKernel()
+        {
+            var kernel = new StandardKernel();
+            RegisterServices(kernel);
+            return kernel;
+        }
+
+        /// <summary>
+        /// Load your modules or register your services here!
+        /// </summary>
+        /// <param name="kernel">The kernel.</param>
+        private void RegisterServices(IKernel kernel)
+        {
+            kernel.Bind<Func<IKernel>>().ToMethod(ctx => () => new Bootstrapper().Kernel);
+            kernel.Bind<IHttpModule>().To<HttpApplicationInitializationHttpModule>();
+            kernel.Bind<IStationFinder>().To<StationFinder>();
+            kernel.Bind<IIndexPageViewModel>().To<IndexPageViewModel>();
+            kernel.Bind<IWebApiStationFinder>().To<WebApiStationFinder>();
+            kernel.Bind<IStationFinderResultPageViewModel>().To<StationFinderResultPageViewModel>();
+            kernel.Bind<IJourneyfinder>().To<JourneyFinder>();
+            kernel.Bind<IWebApiJourneyFinder>().To<WebApiJourneyFinder>();
+            kernel.Bind<IJourneyDetailsPageViewModel>().To<JourneyDetailsPageViewModel>();
+            kernel.Bind<IDatabaseService>().To<DatabaseService>();
+            kernel.Bind<IPreviousJourneysViewModel>().To<PreviousJourneysViewModel>();
+        }
+
+        protected override void OnApplicationStarted()
+        {
+            base.OnApplicationStarted();
+
             AreaRegistration.RegisterAllAreas();
-            FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
-            RouteConfig.RegisterRoutes(RouteTable.Routes);
-            BundleConfig.RegisterBundles(BundleTable.Bundles);
+            RegisterGlobalFilters(GlobalFilters.Filters);
+            RegisterRoutes(RouteTable.Routes);
+            
         }
     }
 }

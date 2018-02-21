c30253c7aed655b576b3187ded936abae50001bc
raqeeb.uddin <raqeeb.uddin@PC-00602>
2018-02-21 14:55:58 +0000

    started SQL connection

---
+++ TfrRedo.DataAccessSql/ISqlAdapter.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TfrRedo.DataAccessSql
+{
+    public interface ISqlAdapter
+    {
+      
+        void Insert();
+        void Update();
+        void Delete();
+    }
+}
---
+++ TfrRedo.DataAccessSql/SqlAdapter.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TfrRedo.DataAccessSql
+{
+    public class SqlAdapter:ISqlAdapter
+    {
+       
+
+        public void Insert()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Delete()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}

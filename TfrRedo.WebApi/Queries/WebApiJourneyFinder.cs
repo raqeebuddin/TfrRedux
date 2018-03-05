055d6a33566ae1155f59fa54ed54d84082fca5d5
raqeeb <raqeebuddin@hotmail.co.uk>
2018-03-05 16:13:14 +0000

    Try catch added to web api journey planner

--- TfrRedo.WebApi/Queries/WebApiJourneyFinder.cs
+++ TfrRedo.WebApi/Queries/WebApiJourneyFinder.cs
@@ -1,8 +1,12 @@
-﻿using System.Net;
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
 using System.Threading.Tasks;
 using Newtonsoft.Json;
 using TfrRedo.Services.Interfaces;
 using TfrRedo.Services.SearchStations.Queries.JourneyFinder;
+using TfrRedo.Services.SearchStations.Queries.stationFinder;
+using TFR.Data.Models.Journey;
 
 namespace TfrRedo.WebApi.Queries
 {
@@ -11,15 +15,31 @@ namespace TfrRedo.WebApi.Queries
         public async Task<JourneyFinderResponseModel> JourneyFinderAsync(string departureStationIcsId,
             string arrivalStationIcsId)
         {
-            var findJourneyApi = string.Format(
+            try
+            {
+                var findJourneyApi = (
                 $"https://api.tfl.gov.uk/journey/journeyresults/{departureStationIcsId}/to/{arrivalStationIcsId}?&mode=tube");
 
-            using (var client = new WebClient())
-            {
-                var json = client.DownloadString(findJourneyApi);
-                var journeys = JsonConvert.DeserializeObject<JourneyFinderResponseModel>(json);
+                using (var client = new WebClient())
+                {
+                    var json = client.DownloadString(findJourneyApi);
+                    var journeys = JsonConvert.DeserializeObject<JourneyFinderResponseModel>(json);
 
-                return await Task.FromResult(journeys);
+                    return await Task.FromResult(journeys);
+                }
+            }
+            catch (Exception e)
+            {
+                var emptySearchDeatils = new JourneyFinderResponseModel();
+
+                emptySearchDeatils.Journeys.Add(new Journey()
+                {
+                    Id = 00000,
+                    StartDateTime = DateTime.Now,
+                    Duration = 0,
+                    ArrivalDateTime = DateTime.Now
+                });
+                return emptySearchDeatils;
             }
         }
     }

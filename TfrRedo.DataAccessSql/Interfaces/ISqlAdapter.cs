57504cca4c05f467f225eaa1e67cec6623ba22b6
raqeeb <raqeebuddin@hotmail.co.uk>
2018-03-06 09:22:56 +0000

    All Interfaces moved to relevent Interfaces folder

---
+++ TfrRedo.DataAccessSql/Interfaces/ISqlAdapter.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TFR.Data.Models.Journey;
+
+namespace TfrRedo.DataAccessSql
+{
+    public interface ISqlAdapter
+    {
+        void Save(Journey journey);
+        void Delete(Journey journey);
+        IEnumerable<Journey> AllJourneys();
+    }
+}
